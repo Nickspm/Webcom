0921fa6db8aef0b11f20953581b5542553264c1f
Nickspm <32419963+Nickspm@users.noreply.github.com>
2017-10-01 00:41:55 +0530

    Updating Class name

---
+++ src/com/webcom/test/ValidLoginTest.java
@@ -0,0 +1,18 @@
+package com.webcom.test;
+
+import org.testng.annotations.Test;
+
+import com.webcom.generic.BaseTest;
+import com.webcom.pages.LoginPage;
+
+public class ValidLoginTest extends BaseTest {
+	@Test(priority =1)
+	public void testValidLogin() throws InterruptedException{
+		LoginPage l = new LoginPage(driver);
+		l.enterUserName(UNAME);
+		l.enterPassword(PWD);
+		l.clickLogin();
+		Thread.sleep(500);
+
+	}
+}
