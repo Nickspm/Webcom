aed17d2a1195291c5209cf285ebe3f5b27858d48
Nickspm <32419963+Nickspm@users.noreply.github.com>
2017-10-03 00:05:13 +0530

    Retry

--- src/com/webcom/interfaces/Itest.java
+++ src/com/webcom/interfaces/Itest.java
@@ -3,10 +3,13 @@ package com.webcom.interfaces;
 public interface Itest {
 	String CHROME_KEY = "webdriver.chrome.driver";
 	String CHROME_VALUE = "E:/Webcom/Webcom/drivers/chromedriver.exe";
-	String URL = "https://webcom5-qa.cxmlpg.com/Default.aspx?id=1";
+	String URL = "https://webcom5-qa.cxmlpg.com/Default.aspx";
 	String XLPATH = "./Data.xlsx";
 	String UNAME = "t_rajatd1@verifone.com";
 	String PWD = "Oct@2017";
-
-
+	//Available Systemid Page Variables 
+	String AVISYSFILTERTYP = "System Id";
+	String SYSTEMID = "30004814";
+	String FilePath = "C:/Users/Nicky/Desktop/Screenshot/test.png";
+	String HomeLink = "https://webcom5-qa.cxmlpg.com/AvailableSystems.aspx";
 }
